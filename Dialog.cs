51672b21a0c4fd118a19a0213af77ecd0affe7f2
kakalasek <honzicekkj@seznam.cz>
2023-12-27 19:46:13 +0100

    Basic dialog structure

---
+++ Dialog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dialog_Data_Structure
+{
+    public class Dialog
+    {
+        private String text;
+        private Dictionary<String, Dialog> options;
+
+        public Dialog()
+        {
+
+        }
+
+        public Dialog(string text)
+        {
+            this.text = text;
+            options = new Dictionary<String, Dialog>();
+        }
+
+        public String Text { get => text; set => text = value; }
+
+        public void AddOption(String text, Dialog? next)
+        {
+            options.Add(text, next);
+        }
+    }
+}
